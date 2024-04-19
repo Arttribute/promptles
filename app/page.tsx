09337898e6347b46e4c0aeaf9b5d54e3accaca2e
bashybaranaba <bashybaranaba@gmail.com>
2024-04-19 16:59:58 +0300

    refactor: change main page to non client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,17 @@
-"use client";
+import Image from "next/image";
 import AccountMenu from "@/components/account/account-menu";
-import { useState, useEffect } from "react";
+
 import GameCard from "@/components/game/game-card";
 
-// async function getGames() {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/games`, {
-//     next: { revalidate: 5 },
-//   });
-//   const data = await res.json();
-//   return data;
-// }
-export default function Home() {
-  const [games, setGames] = useState([]);
-
-  const getGames = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/games`, {
-      next: { revalidate: 5 },
-    });
-    const data = await res.json();
-    setGames(data);
-  };
-
-  useEffect(() => {
-    getGames();
-  }, []);
+async function getGames() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/games`, {
+    next: { revalidate: 5 },
+  });
+  const data = await res.json();
+  return data;
+}
+export default async function Home() {
+  const games = await getGames();
 
   return (
     <div>
