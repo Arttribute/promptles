77652f8f1a535b3c30fb71ac16b3b71258d296a2
bashybaranaba <bashybaranaba@gmail.com>
2024-04-20 01:40:20 +0300

    feat: add games page

---
+++ app/games/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Image from "next/image";
+import AccountMenu from "@/components/account/account-menu";
+import { useState, useEffect } from "react";
+
+import GameCard from "@/components/game/game-card";
+
+export default function Games() {
+  const [games, setGames] = useState([]);
+
+  const getGames = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/games`, {
+      next: { revalidate: 5 },
+    });
+    const data = await res.json();
+    setGames(data);
+  };
+
+  useEffect(() => {
+    getGames();
+  }, []);
+
+  return (
+    <div>
+      <AccountMenu />
+      <div className="flex flex-col items-center justify-center h-screen w-full">
+        <div className="grid grid-cols-12 gap-2">
+          {games &&
+            games.map((game: any) => (
+              <div className="col-span-3" key={game._id}>
+                <GameCard game={game} />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
