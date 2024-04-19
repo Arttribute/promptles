cce17ad55524d2b33c74b3d0d03ce6a74ecb7ce7
bashybaranaba <bashybaranaba@gmail.com>
2024-04-20 01:33:04 +0300

    style: padding 2 for score display

--- components/game/score-display.tsx
+++ components/game/score-display.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export default function ScoreDisplay({ score }: { score: number }) {
   return (
-    <div className="flex flex-col items-center mt-4 px-0.5 py-2 border rounded-lg w-full">
+    <div className="flex flex-col items-center mt-4 p-2 border rounded-lg w-full">
       <p className="text-3xl font-semibold">{score}</p>
       <p className="text-md font-medium ">Score</p>
     </div>
